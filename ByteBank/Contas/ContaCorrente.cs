fc4932ddcd174f235a7108d273da1085f8c8ea9f
Isabella Cruz de Oliveira <isaaholiveira15@gmail.com>
2023-02-10 17:15:15 -0300

    first commit

---
+++ ByteBank/Contas/ContaCorrente.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank
+{
+    public class ContaCorrente
+    {
+        // estrutura que representa o real 
+        // todas as variaveis tem um valor parão de 0 ou null 
+        public int numero_agencia;
+        public string conta;
+        public string agencia;
+        public string nome_agencia;
+        private double saldo = 100;
+        public Cliente titular;
+
+        public void Depositar(double valor)
+        {
+            // método 
+            this.saldo += valor;
+        }
+
+        // se conseguirmos efetuar o saque ele retorna true 
+        public bool Sacar(double valor)
+        {
+            if(valor <= this.saldo)
+            {
+                this.saldo -= valor;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        // o valor adicionado e o destino são os parâmetros 
+        public bool Transferir(double valor, ContaCorrente destino)
+        {
+            if(this.saldo < valor)
+            {
+                return false;
+            }
+            if (valor < 0)
+            {
+                return false;
+            }
+            else
+            {
+                saldo = saldo - valor;
+                destino.saldo = destino.saldo + valor;
+                return true;
+            }
+        }
+
+        // vamos manipular o campo saldo para que nao sejam permitidos que sejam colocados
+        // valores menores que 0
+        // manipula um campo privado da minha classe 
+        public void SetSaldo(double valor)
+        {
+            if(valor < 0)
+            {
+                return;
+            }
+            else
+            {
+                this.saldo = valor;
+            }
+        }
+
+        // vamos criar um metodo publico que acessa e exibe o campo saldo 
+        public double GetSaldo()
+        {
+            return this.saldo;
+        }
+
+        public void DefinirSaldo(double valor)
+        {
+            if (valor < 0)
+            {
+                return;
+            }
+            else
+            {
+                saldo = saldo + valor;
+            }
+        }
+
+        public void ExibirDadosDaConta()
+        {
+            Console.WriteLine("Titular :" + titular);
+            Console.WriteLine("Conta :" + conta);
+            Console.WriteLine("Número Agência :" + numero_agencia);
+            Console.WriteLine("Nome Agência :" + nome_agencia);
+            Console.WriteLine("Saldo: " + saldo);
+        }
+    }
+}
---
+++ ByteBank/Program.cs
@@ -0,0 +1,68 @@
+﻿// vai representar uma conta corrente 
+using ByteBank;
+
+
+// vamos criar um objeto 
+ContaCorrente contaDoAndre = new ContaCorrente();
+//contaDoAndre.titular = "André Silva";
+contaDoAndre.conta = "1010-X";
+// contaDoAndre.saldo = 100;
+
+// Console.WriteLine("Saldo da conta do André " + contaDoAndre.saldo);
+
+contaDoAndre.Depositar(1000);
+// Console.WriteLine("Novo saldo" + contaDoAndre.saldo);
+
+ContaCorrente conta = new ContaCorrente();
+Console.WriteLine($"Titular da conta: {conta.titular}");
+Console.WriteLine($"Número da conta: {conta.conta}");
+Console.WriteLine($"Agência: {conta.agencia}");
+// Console.WriteLine($"Saldo R$ {String.Format("{0:0.00}", conta.saldo)}");
+// Console.WriteLine("Saldo da conta do André pós saque" + contaDoAndre.saldo);
+
+if (contaDoAndre.Sacar(1000) == true)
+{
+    // Console.WriteLine("Saldo da conta" + contaDoAndre.saldo);
+}
+else
+{
+    Console.WriteLine("Saldo insuficiente para saque");
+}
+
+// usamos new para o novo objeto que queremos criar 
+ContaCorrente contaDaIsa = new ContaCorrente();
+//contaDaIsa.titular = "Isabella";
+contaDaIsa.numero_agencia = 17;
+contaDaIsa.conta = "1010-5";
+// contaDaIsa.saldo = 350;
+
+// Console.WriteLine("Saldo da conta da Isa" + contaDaIsa.saldo);
+contaDaIsa.Transferir(50, contaDaIsa);
+// Console.WriteLine("Saldo da conta da Isa" + contaDaIsa.saldo);
+
+Cliente cliente = new Cliente();
+cliente.nome = "Isabella Oliveira";
+cliente.Cpf = "36759212800";
+cliente.profissao = "Dev back-end";
+
+ContaCorrente contaIsa = new ContaCorrente();
+// o titular recebe o objeto de cliente que esta acima
+contaIsa.titular = cliente;
+contaIsa.conta = "1010-X";
+// contaIsa.saldo = 100;
+
+// definir e obter saldo 
+ContaCorrente contaTeste = new ContaCorrente(); 
+// podemos manipular os campos privados dessa classe através dos métodos publicos 
+// contaTeste.saldo = -10; não acesso mais o campo saldo direto 
+contaTeste.SetSaldo(-10); 
+Console.WriteLine("Aqui temos o metodo obter saldo: ");
+Console.WriteLine(contaTeste.GetSaldo());
+
+
+
+
+
+
+
+
